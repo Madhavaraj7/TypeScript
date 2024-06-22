f7777d63b62b718c3e7c1d9b6787f4c59b256eb7
Madhavaraj R <raj266864@gmail.com>
2024-06-22 09:20:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//Infer types(Implicit Types)
+// let userName = "helloworld"
+// userName = 3000
+// console.log(userName);
+//Defining Types (Explicit Types)
+var userName = "Hello world";
+var numbers = 30000;
+var isLogin = true;
+var skills = ["js", "css", "ts"];
+var count = [1, 2, 3, 4, 5];
+var userDeatils = {
+    name: "Madhav",
+    age: 22,
+};
---
+++ index.ts
@@ -0,0 +1,27 @@
+//Infer types(Implicit Types)
+// let userName = "helloworld"
+// userName = 3000
+// console.log(userName);
+
+//Defining Types (Explicit Types)
+// let userName: string = "Hello world";
+// let numbers: number = 30000;
+// let isLogin: boolean = true;
+// let skills: string[] = ["js", "css", "ts"];
+// let count: number[] = [1, 2, 3, 4, 5];
+// let userDeatils: { name: string; age: number } = {
+//   name: "Madhav",
+//   age: 22,
+// };
+
+
+
+//Interface
+let userDeatils: { name: string; age: number } = {
+  name: "Madhav",
+  age: 22,
+};
+
+
+
+
